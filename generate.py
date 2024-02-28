e7efe423b1bc087ff8ed6652efd6c69c4268db93
quantumbagel <jjreder@gmail.com>
2024-02-28 17:33:35 -0500

    tag manager

--- generate.py
+++ generate.py
@@ -156,6 +156,13 @@ HTML_TEMPLATE = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PBLN4JJD');</script>
+<!-- End Google Tag Manager -->
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -169,6 +176,10 @@ HTML_TEMPLATE = """
   <meta name="google-site-verification" content="gLWjWyzJC6D2D_6Pe9HWCRkaWL_rKtxRf05_Dhiu2is" />
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBLN4JJD"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
   <main>
     <aside class="sidebar" data-sidebar>
       <div class="sidebar-info">
